d270ea8b6118af795b823eec2b0c8798f455e547
Eli Brown <eliyellow@gmail.com>
2024-04-17 14:37:42 -0500

    completed first question

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,8 +2,12 @@ console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
 
+myCollection = [];
+console.log(myCollection);
 
-
+function addToCollection(collection, title, artist, yearPublished) {
+  
+}
 
 
 
